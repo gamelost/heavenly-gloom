b8fa9b5a95cf663e12947b12fa1c2cafd89b5c8a
James Tranovich <jtranovich@gmail.com>
2019-03-26 04:35:17 -0700

    list monsters

--- viewer/src/App.js
+++ viewer/src/App.js
@@ -1,32 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import GloomhavenDatabase from './gloomhavenDatabase';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { monsters: [] };
+  }
+
   async componentDidMount() {
-    const response = await fetch('gh.db');
-    const buffer = await response.arrayBuffer();
-    const array = new Uint8Array(buffer);
-    const db = new window.SQL.Database(array);
-    console.log(db.exec('SELECT * FROM monster'));
+    let db = await new GloomhavenDatabase().getInstance();
+    let monsters = await db.getMonsterDeck();
+    this.setState({ monsters });
   }
 
   render() {
+    const monsters = this.state.monsters.map(([id, name]) => <li key={id}>{name}</li>);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload?...test
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <ul>{monsters}</ul>
         </header>
       </div>
     );
