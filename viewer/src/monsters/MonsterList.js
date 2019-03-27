a0eca813399a5bacdf028c15a878e116d0e8c180
Giordon Stark <kratsg@gmail.com>
2019-03-26 22:12:13 -0700

    add a quick filtering to monsterlist

--- viewer/src/monsters/MonsterList.js
+++ viewer/src/monsters/MonsterList.js
@@ -5,7 +5,7 @@ import Table from 'react-bulma-components/lib/components/table';
 class Monsterlist extends Component {
   constructor(props) {
     super(props);
-    this.state = { monsters: [] };
+    this.state = { monsters: [], filterStr: "" };
   }
 
   async componentDidMount() {
@@ -15,21 +15,25 @@ class Monsterlist extends Component {
   }
 
   render() {
-    const monsters = this.state.monsters.map(([id, name]) => {
+    const { filterStr } = this.state;
+    const monsters = this.state.monsters.filter(([id, name]) => (filterStr==="")||name.toLowerCase().includes(filterStr.toLowerCase())).map(([id, name]) => {
       return <tr key={id}>
                <td>{id}</td>
                <td>{name}</td>
              </tr>;
     });
     return (
-      <Table>
-        <thead>
-          <tr><th>ID</th><th>Name</th></tr>
-        </thead>
-        <tbody>
-          {monsters}
-        </tbody>
-      </Table>
+      <div>
+        <input type="text" value={ filterStr } onChange={ e => this.setState({ filterStr: e.target.value }) } />
+        <Table>
+          <thead>
+            <tr><th>ID</th><th>Name</th></tr>
+          </thead>
+          <tbody>
+            {monsters}
+          </tbody>
+        </Table>
+      </div>
     );
   }
 }
