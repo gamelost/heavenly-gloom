0573e590fc17cb0890734814dcf034ae4af3aa03
James Tranovich <jtranovich@gmail.com>
2019-03-31 09:56:55 -0700

    Restructure monster ability attacks

--- viewer/src/monsters/MonsterAbilityCard.js
+++ viewer/src/monsters/MonsterAbilityCard.js
@@ -6,63 +6,76 @@ import Tag from 'react-bulma-components/lib/components/tag';
 class MonsterAbilityCard extends Component {
   constructor(props) {
     super(props);
-    const deckId = props.deckId;
-    const deckName = props.deckName;
-    const [card_number, shuffle, initiative, attacksRaw, image] = props.ability;
+    const { deckId, deckName, macros, ability } = props;
+    const [card_number, shuffle, initiative, attacksRaw, image] = ability;
     const attacks = JSON.parse(attacksRaw);
-    const macros = props.macros;
     this.state = { deckId, deckName, card_number, shuffle, initiative, attacks, image, macros };
   }
 
+  // TODO simplify MonsterAbilityMacroImage
+  // TODO proper sections
+  // TODO multiple arrays for lines
+
   render() {
-      const body = this.state.attacks.map((attack, index) => {
-        let output = attack;
-
-        const macros = attack.match(/%[^%]+%/g) || [];
-        for(const macro of macros) {
-          const what = typeof(output) === 'string' ? output : output.pop();
-          const result = what.split(macro).reduce((prev, current, i) => {
-            if (i==0) {
-              return [current];
-            }
-            return prev.concat(<MonsterAbilityMacroImage key={'macro-'+i} macro={this.state.macros[macro]} />, current);
-          }, []);
-
-          if (typeof(output) === 'string') {
-            output = result;
-          } else {
-            output.push(result);
+    const body = this.state.attacks.map(({ block }) => {
+      return block.map(({ type, line }, index) => {
+        let output;
+        switch(type) {
+        case 'description':
+          output = line;
+          break;
+        case 'action':
+          {
+            const [macro, modifier] = line;
+            output = <span style={{ display: 'flex' }}>
+                       <MonsterAbilityMacroImage key={macro} macro={this.state.macros[macro]}/>
+                       {modifier > 0 ? `+ ${modifier}` : `- ${Math.abs(modifier)}`}
+                     </span>;
           }
-        }
-
-        // account for "block" -- TODO could be better
-        if (attack.startsWith('^')) {
-          if (typeof(output) === 'string') {
-            output = output.substr(1);
-          } else {
-            output.shift(1);
+          break;
+        case 'effect':
+          {
+            let macro = line;
+            output = <MonsterAbilityMacroImage key={macro} macro={this.state.macros[macro]}/>;
           }
-          output = <small style={{ display: 'flex' }}>{output}</small>;
+          break;
+        case 'setElement':
+          {
+            let [macro] = line;
+            output = <MonsterAbilityMacroImage key={macro} macro={this.state.macros[macro]}/>;
+          }
+          break;
+        case 'useElement':
+          {
+            let [macro1, macro2] = line;
+            output = <MonsterAbilityMacroImage key={macro1} macro={this.state.macros[macro1]}/>;
+            output = <MonsterAbilityMacroImage key={macro2} macro={this.state.macros[macro2]}/>;
+          }
+          break;
+        default:
+          console.warn('unknown type', type); break;
         }
 
         return <div className="ability-entry" style={{ display: 'flex', margin: '0 0 0.5em 0', justifyContent: 'center' }} key={index}>{output}</div>;
       });
-      return (
-        <Card style={{ color: 'white', background: 'black', borderRadius: '0.75em', overflow: 'hidden' }}>
-          <Card.Header style={{ background: '#666' }}>
-            <Tag className="card-initiative" style={{ borderRadius: 0 }}>{this.state.initiative}</Tag>
-            <Card.Header.Title style={{ fontWeight: 'bold', color: 'white', justifyContent: 'center' }} key={this.state.deckName}>
-              {this.state.deckName}
-            </Card.Header.Title>
-          </Card.Header>
-          <Card.Content>
-            {body}
-          </Card.Content>
-          <Card.Footer>
-            <Tag className="card-number" style={{ borderRadius: 0 }}>{this.state.card_number}</Tag>
-          </Card.Footer>
-        </Card>
-      )
-    }
+    });
+
+    return (
+      <Card style={{ color: 'white', background: 'black', borderRadius: '0.75em', overflow: 'hidden' }}>
+        <Card.Header style={{ background: '#666' }}>
+          <Tag className="card-initiative" style={{ borderRadius: 0 }}>{this.state.initiative}</Tag>
+          <Card.Header.Title style={{ fontWeight: 'bold', color: 'white', justifyContent: 'center' }} key={this.state.deckName}>
+            {this.state.deckName}
+          </Card.Header.Title>
+        </Card.Header>
+        <Card.Content>
+          {body}
+        </Card.Content>
+        <Card.Footer>
+          <Tag className="card-number" style={{ borderRadius: 0 }}>{this.state.card_number}</Tag>
+        </Card.Footer>
+      </Card>
+    );
+  }
 }
 export default MonsterAbilityCard;
