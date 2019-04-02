903d3d574427861e903f3e8b80aaf02a75c5be06
James Tranovich <jtranovich@gmail.com>
2019-04-01 17:28:09 -0700

    mobile support

--- viewer/src/Header.js
+++ viewer/src/Header.js
@@ -1,37 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from 'react-bulma-components/lib/components/navbar';
 import Image from 'react-bulma-components/lib/components/image';
 import './Header.css';
 
-export default function header() {
-  return (
-    <Navbar>
-      <Navbar.Brand>
-        <Navbar.Item href="/">
-          <Image
-            src="http://esotericsoftware.com/files/ghh/logo.png"
-            alt="Gloomhaven Logo"
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+
+  render() {
+    return (
+      <Navbar>
+        <Navbar.Brand>
+          <Navbar.Item href="/">
+            <Image
+              src="http://esotericsoftware.com/files/ghh/logo.png"
+              alt="Gloomhaven Logo"
+            />
+          </Navbar.Item>
+          <Navbar.Burger
+            className={this.state.open ? 'is-active' : ''}
+            onClick={() =>
+                     this.setState(state => ({
+                       open: !state.open
+                     }))
+                    }
           />
-        </Navbar.Item>
-      </Navbar.Brand>
-      <Navbar.Menu>
-        <Navbar.Container>
-          <Navbar.Item renderAs="div" className="has-dropdown is-hoverable">
-            <Navbar.Item>Monsters</Navbar.Item>
-            <Navbar.Dropdown>
-              <Navbar.Item href="/monsters">List</Navbar.Item>
-              <Navbar.Item href="/monsters/deck">Deck</Navbar.Item>
-            </Navbar.Dropdown>
-          </Navbar.Item>
-          <Navbar.Item renderAs="div" className="has-dropdown is-hoverable">
-            <Navbar.Item>Scenarios</Navbar.Item>
-            <Navbar.Dropdown>
-              <Navbar.Item href="/scenarios">List</Navbar.Item>
-              <Navbar.Item href="/scenarios/worldmap">World Map</Navbar.Item>
-            </Navbar.Dropdown>
-          </Navbar.Item>
-        </Navbar.Container>
-      </Navbar.Menu>
-    </Navbar>
-  );
+        </Navbar.Brand>
+        <Navbar.Menu className={this.state.open ? 'is-active' : ''}>
+          <Navbar.Container>
+            <Navbar.Item renderAs="div" className="has-dropdown is-hoverable">
+              <Navbar.Item>Monsters</Navbar.Item>
+              <Navbar.Dropdown>
+                <Navbar.Item href="/monsters">List</Navbar.Item>
+                <Navbar.Item href="/monsters/deck">Deck</Navbar.Item>
+              </Navbar.Dropdown>
+            </Navbar.Item>
+            <Navbar.Item renderAs="div" className="has-dropdown is-hoverable">
+              <Navbar.Item>Scenarios</Navbar.Item>
+              <Navbar.Dropdown>
+                <Navbar.Item href="/scenarios">List</Navbar.Item>
+                <Navbar.Item href="/scenarios/worldmap">World Map</Navbar.Item>
+              </Navbar.Dropdown>
+            </Navbar.Item>
+          </Navbar.Container>
+        </Navbar.Menu>
+      </Navbar>
+    );
+  }
 }
+
+export default Header;
