f8db766202a2dffa0fa4172bc9d062d69e24af35
James Tranovich <jtranovich@gmail.com>
2019-04-04 04:42:57 -0700

    example model/schema

--- graphql-server/src/main.rs
+++ graphql-server/src/main.rs
@@ -1,13 +1,53 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(decl_macro, proc_macro_hygiene)]
 
+extern crate juniper;
+extern crate juniper_rocket;
 #[macro_use]
 extern crate rocket;
 
+mod model;
+mod schema;
+
+use crate::model::Database;
+use juniper::{EmptyMutation, RootNode};
+use rocket::response::content;
+use rocket::State;
+
+type Schema = RootNode<'static, Database, EmptyMutation<Database>>;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn graphiql() -> content::Html<String> {
+    juniper_rocket::graphiql_source("/graphql")
+}
+
+#[get("/graphql?<request>")]
+fn get_graphql_handler(
+    context: State<Database>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute(&schema, &context)
+}
+
+#[post("/graphql", data = "<request>")]
+fn post_graphql_handler(
+    context: State<Database>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute(&schema, &context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .manage(Database::new())
+        .manage(Schema::new(
+            Database::new(),
+            EmptyMutation::<Database>::new(),
+        ))
+        .mount(
+            "/",
+            routes![graphiql, get_graphql_handler, post_graphql_handler],
+        )
+        .launch();
 }
