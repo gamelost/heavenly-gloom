abb0e909ee67dee560045c34597ec8b6fe44e408
James Tranovich <jtranovich@gmail.com>
2019-04-11 18:53:38 -0700

    simplify error handling

--- graphql-server/src/rules/game_state_rules.rs
+++ graphql-server/src/rules/game_state_rules.rs
@@ -8,17 +8,12 @@ impl GameStateRules {
     pub fn change_prosperity_level(mutations: &Mutations, level: i32) -> FieldResult<Mutations> {
         match level {
             1...9 => {
-                let pool = &mutations.pool;
-                match GameState::update_prosperity_level(pool, level) {
-                    Ok(_) => {
-                        let game = GameState::refresh(pool);
-                        Ok(Mutations {
-                            game,
-                            pool: mutations.pool.clone(),
-                        })
-                    }
-                    Err(error) => Err(FieldError::new(error.to_string(), Value::null())),
-                }
+                GameState::update_prosperity_level(&mutations.pool, level)?;
+                let game = GameState::refresh(&mutations.pool);
+                Ok(Mutations {
+                    game,
+                    pool: mutations.pool.clone(),
+                })
             }
             _ => Err(FieldError::new("Invalid prosperity level", Value::null())),
         }
