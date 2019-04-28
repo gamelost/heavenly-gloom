a36e892965ab878ffca3108953fe31a9a468457c
Anja Berens <pharaun666@gmail.com>
2019-04-28 15:38:21 -0700

    Improve player state table + add player_item state

--- db/state.sql
+++ db/state.sql
@@ -13,13 +13,33 @@ DROP TABLE IF EXISTS player;
 CREATE TABLE player(
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL UNIQUE,
-  hit_points INTEGER NOT NULL,
-  experience INTEGER NOT NULL,
-  gold INTEGER NOT NULL
+  -- Start scenario Specific
+  current_hit_points INTEGER NOT NULL,
+  current_loot INTEGER NOT NULL DEFAULT 0,
+  -- Current player status (imobilized, etc)
+  -- End scenario Specific
+  total_experience INTEGER NOT NULL,
+  gold INTEGER NOT NULL,
+  character_id INTEGER NOT NULL UNIQUE,
+  FOREIGN KEY(character_id) REFERENCES character(id)
 );
 
-INSERT INTO player(name, hit_points, experience, gold) VALUES ('mr. pink', 10, 50, 500);
-INSERT INTO player(name, hit_points, experience, gold) VALUES ('mrs. purple', 8, 45, 100);
+INSERT INTO player(name, character_id, current_hit_points, total_experience, gold) VALUES ('mr. pink', 1, 10, 50, 500);
+INSERT INTO player(name, character_id, current_hit_points, total_experience, gold) VALUES ('mrs. purple', 2, 8, 45, 100);
+
+DROP TABLE IF EXISTS player_item;
+CREATE TABLE player_item(
+  id INTEGER PRIMARY KEY,
+  player_id INTEGER NOT NULL,
+  item_card_id INTEGER NOT NULL,
+  FOREIGN KEY(player_id) REFERENCES player(id),
+  FOREIGN KEY(item_card_id) REFERENCES item_card(id)
+);
+
+INSERT INTO player_item(player_id, item_card_id) VALUES (1, 1);
+INSERT INTO player_item(player_id, item_card_id) VALUES (1, 2);
+INSERT INTO player_item(player_id, item_card_id) VALUES (2, 1);
+INSERT INTO player_item(player_id, item_card_id) VALUES (2, 3);
 
 -- Cos Anja NFS home sucks
 COMMIT;
